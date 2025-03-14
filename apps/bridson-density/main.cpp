512d5028d57c5d41ee9be0f279be7db95586c9a1
Ethan Dobrowolski <ethan.dobrowolski@gmail.com>
2025-03-14 15:17:57 -0400

    fix name

---
+++ apps/bridson-density/main.cpp
@@ -0,0 +1,6 @@
+#include "bridson_density.hpp"
+
+int main() {
+    BridsonDensity::launch<BridsonDensity>(800, 600, "Bridson Density-based",
+                                           60.0f, "apps/bridson-density");
+}
--- apps/bridson/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "bridson_density.hpp"
-
-int main() {
-    BridsonDensity::launch<BridsonDensity>(800, 600, "Bridson Muller-Fischer",
-                                           60.0f, "apps/bridson-mullerfischer");
-}
